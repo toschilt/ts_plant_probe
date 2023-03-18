2932615920ab9718db34dc6964bf6ab780daaebe
toschilt <lt.toschi@gmail.com>
2023-03-18 17:53:13 -0500

    Adding extrinsic information only affects the 3D point cloud. After that, the other scene properties are recalculated as they were at the first time. Needs to be refactored to spare computational power

--- features_3d/scene.py
+++ features_3d/scene.py
@@ -76,6 +76,10 @@ class AgriculturalScene:
                 desired translation in the x, y and z axis, respectively.
             rotation: a tuple containing three floats describing the 
                 desired yaw, pitch and roll rotations.
+
+        #TODO: crop and ground plane calculation are done before and after
+            adding extrinsics. Refactor constructors to spare computational
+            power.
         """
         if self.extrinsics is None:
             # Get the extrinsics matrix.
@@ -95,35 +99,36 @@ class AgriculturalScene:
             t = np.array([translation[0], translation[1], translation[2]])[:, None]
             self.extrinsics = np.vstack((np.hstack((R, t)), [0, 0, 0, 1]))
 
+        # Modfying the ground plane
+        for p_3d in self.ground_plane.ps_3d:
+            p_3d = self._apply_extrinsics_to_3D_vector(p_3d, self.extrinsics)
+
+        self.ground_plane.average_point = np.average(
+            self.ground_plane.ps_3d,
+            axis=0
+        )
+        self.ground_plane.ground_vectors = self.ground_plane._get_principal_components(
+            self.ground_plane.ps_3d
+        )
+        self.ground_plane.normal_vector = np.cross(
+            self.ground_plane.ground_vectors[0],
+            self.ground_plane.ground_vectors[1]
+        )
+        self.ground_plane.coeficients = self.ground_plane._get_plane_coefficients(
+            self.ground_plane.normal_vector,
+            self.ground_plane.average_point
+        )
+
         # Modfying the crops
         for crop in self.crop_group.crops:
             for p_3d in crop.ps_3d:
                 p_3d = self._apply_extrinsics_to_3D_vector(p_3d, self.extrinsics)
 
-            crop.average_point = self._apply_extrinsics_to_3D_vector(crop.average_point, self.extrinsics)
-            crop.crop_vector = self._apply_extrinsics_to_3D_vector(crop.crop_vector, self.extrinsics)
-            crop.emerging_point = self._apply_extrinsics_to_3D_vector(crop.emerging_point, self.extrinsics)
-
-        # Modfying the ground plane
-        for p_3d in self.ground_plane.ps_3d:
-            p_3d = self._apply_extrinsics_to_3D_vector(p_3d, self.extrinsics)
-
-        for vector in self.ground_plane.ground_vectors:
-            vector = self._apply_extrinsics_to_3D_vector(vector, self.extrinsics)
-
-        self.ground_plane.normal_vector = self._apply_extrinsics_to_3D_vector(
-            self.ground_plane.normal_vector,
-            self.extrinsics)
-        
-        self.ground_plane.average_point = self._apply_extrinsics_to_3D_vector(
-            self.ground_plane.average_point,
-            self.extrinsics
-        )
-        
-        self.ground_plane.coeficients = self.ground_plane._get_plane_coefficients(
-            self.ground_plane.normal_vector,
-            self.ground_plane.average_point
-        )
+            crop.average_point = np.average(crop.ps_3d, axis=0)
+            crop.crop_vector = crop._get_principal_component(crop.ps_3d)
+            crop.emerging_point = crop.find_emerging_point(
+                self.ground_plane
+            )
 
     def plot(
         self,
