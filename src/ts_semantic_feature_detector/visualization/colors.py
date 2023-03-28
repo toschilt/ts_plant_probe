d4da36d011da53ae8d4f585681983f3041433480
toschilt <lt.toschi@gmail.com>
2023-03-28 08:58:39 -0500

    Adds a abstraction for coloring clusters

---
+++ src/ts_semantic_feature_detector/visualization/colors.py
@@ -0,0 +1,9 @@
+import matplotlib.colors as mcolors
+
+def get_color_from_cluster(cluster):
+    """
+    Returns a string containing a different color for each cluster number.
+
+    Colors are returned in hexadecimal format.
+    """
+    return list(mcolors._colors_full_map.values())[cluster + 10]
