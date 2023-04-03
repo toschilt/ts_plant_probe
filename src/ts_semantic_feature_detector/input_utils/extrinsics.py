003c306db806f70fdb6b163017c73775216f12d7
toschilt <lt.toschi@gmail.com>
2023-04-03 10:55:15 -0500

    Adds transformation between body frame and camera frame

--- src/ts_semantic_feature_detector/input_utils/extrinsics.py
+++ src/ts_semantic_feature_detector/input_utils/extrinsics.py
@@ -9,7 +9,7 @@ from scipy.spatial.transform import Rotation
 def get_extrinsics(
     ekf: List,
     imu: List
-) -> Tuple[List, List]:
+) -> Tuple[List, List, List, List]:
     """
     Get the extrinsics information from IMU and EKF.
 
@@ -34,15 +34,17 @@ def get_extrinsics(
     ekf = ekf[2:]
     imu = imu[2:]
 
-    position = [ekf[0], ekf[1], ekf[2]]
+    pos_world_body = [ekf[0], ekf[1], ekf[2]]
 
     ekf_quat = [ekf[3], ekf[4], ekf[5], ekf[6]]
     imu_quat = [imu[0], imu[1], imu[2], imu[3]]
     ekf_euler = Rotation.from_quat(ekf_quat).as_euler('xyz')
     imu_euler = Rotation.from_quat(imu_quat).as_euler('xyz')
-    orientation = [imu_euler[0], imu_euler[1], ekf_euler[2]]
+    # orientation = [imu_euler[0], imu_euler[1], ekf_euler[2]]
+    orient_world_body = [ekf_euler[0], ekf_euler[1], ekf_euler[2]]
 
-    # Fixes the orientation of the feature detector compared to the EKF
-    orientation[1] += np.pi/2
+    pos_camera_body = [0.06, 0.24, -0.018]
+    orient_camera_body_quat = [-0.44643, 0.53021, -0.52716, -0.49160]
+    orient_camera_body_euler = Rotation.from_quat(orient_camera_body_quat).as_euler('xyz')
 
-    return position, orientation
+    return pos_world_body, orient_world_body, pos_camera_body, orient_camera_body_euler
