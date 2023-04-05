e89ea93fe483a607a449f8c1b1b2fda375942ef0
toschilt <lt.toschi@gmail.com>
2023-04-05 10:38:18 -0500

    Simple class and executable to visualize the pipeline times

---
+++ src/ts_semantic_feature_detector/perfomance/visualization/log.py
@@ -0,0 +1,54 @@
+"""
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class TimerVisualizer:
+    """
+    Visualizes the times of each step of the pipeline
+    """
+
+    def __init__(
+        self,
+        times_file: str,
+    ):
+        """
+        Initializes the visualizer
+
+        Args:
+            times_file: a string containing the path to the times file
+        """
+
+        self.times_file = times_file
+
+        f = open(self.times_file)
+        self.headers = f.readline()
+        self.headers = self.headers.split(',')
+
+        self.data = np.loadtxt(
+            self.times_file,
+            delimiter=',',
+            skiprows=1,
+            usecols=range(1, len(self.headers) - 1)
+        )
+
+    def plot(
+        self
+    ):
+        """
+        Plots the times of each step of the pipeline
+        """
+
+        plt.figure()
+        for header, data in zip(self.headers, self.data.T):
+            if (data > 0.001).all():
+                plt.plot(data, label=header)
+        plt.legend(loc='best')
+        plt.show()
+
+if __name__ == '__main__':
+    visualizer = TimerVisualizer(
+        '/home/daslab/Documents/dev/catkin_ws/src/ts_semantic_feature_detector/output/times.txt'
+    )
+    visualizer.plot()
